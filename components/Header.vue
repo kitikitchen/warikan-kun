2dabdf2d19886a98a230564bacc4f374a3204b44
Yoshihara Fumio <yoshihara@un-t.com>
2019-11-20 15:40:51 +0900

    scssファイル追加

---
+++ components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <p class="title">割り勘<span>くん</span></p>
+    <h1 v-ripple class="title">warikan-kun</h1>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 600;
+  font-size: vw(40);
+  color: #35495e;
+  letter-spacing: 1px;
+
+  span {
+    font-size: 0.7em;
+  }
+
+  @include breakpoint-up(sm) {
+    background: turquoise;
+    &::after {
+      content: "：SM";
+    }
+  }
+  @include breakpoint-up(md) {
+    background: violet;
+    &::after {
+      content: "：MD";
+    }
+  }
+  @include breakpoint-up(lg) {
+    background: wheat;
+    &::after {
+      content: "：LG";
+    }
+  }
+
+}
+</style>
