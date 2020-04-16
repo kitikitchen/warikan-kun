aeb135a6a07fb79815711cc16f7133165edb9a64
Imanishi Toshifumi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-04-16 15:49:03 +0900

    合計金額コンポーネントのスタイル調整

--- components/AppTotalPrice.vue
+++ components/AppTotalPrice.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="total_price">
-    <div class="total_price_price">
-      <p>合計金額</p>
-      <input
-        v-model.number="totalPrice"
-        type="number"
-        placeholder="¥0"
-        @change="updateTotalPrice"
-      >
+  <div class="totalprice">
+    <div class="totalprice_fieldset">
+      <label>
+        <span class="totalprice_label">合計金額</span>
+        <input
+          v-model.number="totalPrice"
+          class="totalprice_textbox"
+          type="number"
+          placeholder="¥0"
+          @change="updateTotalPrice"
+        >
+      </label>
     </div>
-    <div v-if="isSuccess" class="total_price_remainder">
+    <div v-if="isSuccess" class="totalprice_remainder">
       余り<br>¥{{ remainder }}
     </div>
   </div>
@@ -39,35 +42,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.total_price {
-  position: relative;
+.totalprice {
   padding: 10px vw(40);
-
-  p {
-    display: flex;
-    align-items: center;
-    position: absolute;
-    top: 0;
-    left: 40px;
-    bottom: 0;
-    margin: auto;
-    line-height: 1;
-    font-size: 12px;
-  }
-
-  input {
-    width: 100%;
-    text-align: right;
-    border: 2px solid $color-gray;
-    background: $color-gray-light;
-    padding: 5px 20px 5px 86px;
-    font-size: 24px;
-    transition: ease 0.5s;
-
-    .is-success & {
-      background: $color-white;
-    }
-  }
 
   .is-success & {
     margin-top: 20px;
@@ -82,16 +58,45 @@ export default {
   }
 }
 
-.total_price_price {
+.totalprice_fieldset {
+  > label {
+    position: relative;
+  }
+
   .is-success & {
     width: 80%;
   }
 }
 
-.total_price_remainder {
+.totalprice_label {
+  position: absolute;
+  top: 0;
+  left: 20px;
+  bottom: 0;
+  margin: auto;
+  line-height: 1;
+  font-size: 12px;
+}
+
+.totalprice_textbox {
+  width: 100%;
+  text-align: right;
+  border: 2px solid $color-gray;
+  background: $color-gray-light;
+  padding: 5px 20px 5px 86px;
+  font-size: 24px;
+  transition: ease 0.5s;
+
+  .is-success & {
+    background: $color-white;
+  }
+}
+
+.totalprice_remainder {
   color: $color-white;
   text-align: center;
   line-height: 1.4;
+
   .is-success & {
     padding-left: 20px;
     width: 20%;
